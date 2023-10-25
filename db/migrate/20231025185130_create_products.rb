539bac92f5f732226b6c67cbb882c71184038c70
Kuba Jądrzak <kuba.jadrzak@gmail.com>
2023-10-25 21:06:20 +0200

    initial commit

---
+++ db/migrate/20231025185130_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.text :title
+      t.decimal :price, precision: 8, scale: 2
+      t.text :description
+      t.timestamps
+    end
+  end
+end
