925aafa24e7b8437893cf49479b15b2a22eab525
Kuba Jądrzak <kuba.jadrzak@gmail.com>
2024-02-12 18:13:34 +0100

    smtp config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,9 +63,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost' }
+  
+  # SMTP settings for Gmail
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    user_name:            "adversalit@gmail.com",
+    password:             smtp_password,
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.force_ssl = false
