23ad9954cec29d009fc2aeb1b207d2d9100ef854
Kuba Jądrzak <kuba.jadrzak@gmail.com>
2024-02-26 21:04:51 +0100

    production configuration 4

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,7 +3,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   def show
     super do |resource|
       if resource.confirmed?
-        redirect_to Rails.application.config.confirmation_redirect_url allow_other_host: true and return
+        redirect_to Rails.application.config.confirmation_redirect_url, allow_other_host: true and return
       end
     end
   end
