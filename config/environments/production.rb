c90d88fa43abfd20202128adbc15080fb5687209
Kuba Jądrzak <kuba.jadrzak@gmail.com>
2024-02-26 20:38:57 +0100

    production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,13 +6,13 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = true
+  config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable server timing
   config.server_timing = true
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'adversal.it', port: '3000'}
+  config.action_mailer.default_url_options = { host: 'adversal.it', protocol: 'http'}
   smtp_password = Rails.application.credentials[:password]
   
   # SMTP settings for Gmail
@@ -76,7 +76,6 @@ Rails.application.configure do
     authentication:       "plain",
     enable_starttls_auto: true
   }
-
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.force_ssl = false
